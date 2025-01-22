834801aae21ffd5bcf82eab5fd6a9af69e1e15bc
The-Nauman-Mirza <naumanmirza131@gmail.com>
2025-01-22 18:30:17 +0530

    deploy6

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/features" element={<Features />} />
